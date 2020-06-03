d17d8caa4ada590f772e1c2cb86fce4bc948f787
Yash27112006 <yashu53421@gmail.com>
2020-06-03 17:10:01 +0530

    Form

--- js/Player.js
+++ js/Player.js
@@ -1,5 +1,9 @@
 class Player {
-  constructor(){}
+  constructor(){
+    this.index = null;
+    this.name = null;
+    this.distance = 0;
+  }
 
   getCount(){
     var playerCountRef = database.ref('playerCount');
@@ -7,17 +11,24 @@ class Player {
       playerCount = data.val();
     })
   }
-
+   
   updateCount(count){
     database.ref('/').update({
       playerCount: count
     });
   }
 
-  update(name){
-    var playerIndex = "player" + playerCount;
+  update(){
+    var playerIndex = "players/player" + this.index;
     database.ref(playerIndex).set({
-      name:name
+      name : this.name,
+      distance : this.distance
     });
+  }
+  static getPlayerInfo(){
+    var players = database.ref('players');
+    players.on("value",(data)=>{
+      allPlayers = data.val();
+    })
   }
 }
